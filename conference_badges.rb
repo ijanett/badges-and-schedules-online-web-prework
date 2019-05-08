10604fa91cdd0dbd4296ef23823fa32d3ec22288
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-08 18:07:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker(name)
     puts "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  attendees.each do |attendee|
+    puts "Hello, my name is #{attendee}."
+  end
+end
