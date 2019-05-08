81aeeb3676312c5011518b7979066c3ad816fede
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-08 18:21:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,9 @@ def batch_badge_creator(attendees)
     "Hello, my name is #{attendee}."
   end
 end
+
+def assign_rooms(attendees)
+  attendees.each_with_index do |attendee, room|
+    "Hello, #{attendee}! You'll be assigned to room #{room}."
+  end
+end
